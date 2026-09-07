b04f3022250fcf064f09e76154bd5e91d8ad6e1f
Sara Oueld Beyad <oueldbeyad.sara@gmail.com>
2026-09-06 22:07:47 -0400

    changing random UUID to fixed one

--- src/com/sara/user/UserDAO.java
+++ src/com/sara/user/UserDAO.java
@@ -8,11 +8,11 @@ public class UserDAO {
 
     static {
         users = new User[] {
-                new User(UUID.randomUUID(), "Sara"),
-                new User(UUID.randomUUID(), "Israe"),
-                new User(UUID.randomUUID(), "Ayoub"),
-                new User(UUID.randomUUID(), "Samira"),
-                new User(UUID.randomUUID(), "Abderrahim"),
+                new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Sara"),
+                new User(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"), "Israe"),
+                new User(UUID.fromString("3f2504e0-4f89-41d3-9a0c-0305e82c3301"), "Ayoub"),
+                new User(UUID.fromString("6ba7b810-9dad-41d1-80b4-00c04fd430c8"), "Samira"),
+                new User(UUID.fromString("123e4567-e89b-42d3-a456-426614174000"), "Abderrahim"),
         };
     }
 
